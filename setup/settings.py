7b78e42624a198dabe7500789f1cf8cfad7eadef
dy071 <diana.ribeiro071@gmail.com>
2026-05-16 12:46:24 -0300

    finalização

--- setup/settings.py
+++ setup/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.mysql',
 
         'NAME': 'estoque_db',
 
